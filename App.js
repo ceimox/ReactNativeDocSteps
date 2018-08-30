b7e1dfbf77b52a67985f7979dd4826bab58b026a
cesar hernandez <ceimox19@gmail.com>
2018-08-29 22:40:49 -0500

    initial to props

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={styles.container}>
+        <Text>Hola Leidy mi hermosa bb</Text>
+        <Text>Te amo mucho.</Text>
+        <Greeting name='yupi' />
+        <Bananas />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
